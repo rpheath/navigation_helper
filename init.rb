f0bf6cd886a0fc57ad6f945a620d467879ee1391
rpheath <rpheath@gmail.com>
2008-04-13 01:18:39 -0400

    spacing back to normal (still testing)

--- init.rb
+++ init.rb
@@ -1,4 +1,2 @@
-ActionView::Base.send :include, 
-  NavigationHelper::InstanceMethods
-ActionController::Base.send :extend, 
-  NavigationHelper::ClassMethods
+ActionView::Base.send :include, NavigationHelper::InstanceMethods
+ActionController::Base.send :extend, NavigationHelper::ClassMethods
