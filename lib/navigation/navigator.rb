20768054cbbe5738dc9f3ee3024b5397ac32a9fc
rpheath <rpheath@gmail.com>
2008-11-06 08:23:52 -0500

    just formatting

--- lib/navigation/navigator.rb
+++ lib/navigation/navigator.rb
@@ -6,14 +6,66 @@ module RPH
     # the navigation helper generates a new instance of this class
     class Navigator	
       include Error
-
+      
       def initialize(sections, options)
         @sections = sections
         @options = options
         validate_sections!
         fill_subtitles if has_subtitles?
       end
+      
+    private      
+      # loads the SUBTITLES constant with key/value relationships for section/subtitle
+      def fill_subtitles
+        @sections.in_groups_of(2) { |group| SUBTITLES[group.first] = group.last }
+      end
 	
+      # assumed that if all items are symbols, subtitles are not present
+      def has_subtitles?
+        !@sections.all? { |section| section.is_a?(Symbol) }
+      end
+			
+      def requires_authorization?
+        @options.has_key?(:authorize) && !@options[:authorize].blank?
+      end
+	
+      def authorize_all?(methods)
+        return false if methods.blank?
+        methods.size == 1 && methods.first == :all
+      end
+  
+      def sections_is_an_array?
+        @sections.is_a?(Array)
+      end
+  
+      def one_to_one_match_for_sections_and_subtitles?
+        @sections.size % 2 == 0
+      end
+
+      def valid_types?
+        !(@sections.first.is_a?(String) || @sections.all? { |section| section.is_a?(String) })
+      end
+	  
+	  protected
+	    # distinguishes between sections and subtitles, returning sections
+      def parse(sections)
+        temp = []
+        sections.each_with_index do |section, index|
+          temp << section if section.is_a?(Symbol) && index.even?
+        end
+        temp
+      end
+  
+      # ensures that the links passed in are valid
+      def validate_sections!
+        raise(InvalidSections, InvalidSections.message) unless sections_is_an_array?
+        if has_subtitles?
+          raise(InvalidArrayCount, InvalidArrayCount.message) unless one_to_one_match_for_sections_and_subtitles?
+        end
+        raise(InvalidType, InvalidType.message) unless valid_types?
+      end
+	  
+	  public
       # will return the links passed in, removing subtitles if they exist
       def sections
         has_subtitles? ? parse(@sections) : @sections
@@ -37,11 +89,7 @@ module RPH
       # returns the method used for checking if links are allowed to
       # be added to the list (defaults to <tt>logged_in?</tt> method)
       def authorization_method
-        method = :logged_in?
-        if @options.has_key?(:authorize)
-          method = @options[:with] if @options.has_key?(:with)
-        end
-        method
+        @options.has_key?(:authorize) && @options.has_key?(:with) ? @options[:with] : :logged_in?
       end
 	
       # returns an array of the methods that require authorization
@@ -58,57 +106,6 @@ module RPH
       def authorized_css
         return '' if methods_to_authorize.blank?
         @options[:authorized_css] ||= 'authorized_nav_link'
-      end
-  
-      protected
-      # distinguishes between sections and subtitles, returning sections
-      def parse(sections)
-        temp = []
-        sections.each_with_index do |section, index|
-          temp << section if section.is_a?(Symbol) && index.even?
-        end
-        temp
-      end
-  
-      # ensures that the links passed in are valid
-      def validate_sections!
-        raise(InvalidSections, InvalidSections.message) unless sections_is_an_array?
-        if has_subtitles?
-          raise(InvalidArrayCount, InvalidArrayCount.message) unless one_to_one_match_for_sections_and_subtitles?
-        end
-        raise(InvalidType, InvalidType.message) unless valid_types?
-      end
-    
-      private      
-      # loads the SUBTITLES constant with key/value relationships for section/subtitle
-      def fill_subtitles
-        @sections.in_groups_of(2) { |group| SUBTITLES[group[0]] = group[1] }
-      end
-	
-      # assumed that if all items are symbols, subtitles are not present
-      def has_subtitles?
-        !@sections.all? { |section| section.is_a?(Symbol) }
-      end
-			
-      def requires_authorization?
-        @options.has_key?(:authorize) && !@options[:authorize].blank?
-      end
-	
-      def authorize_all?(methods)
-        return false if methods.blank?
-        methods.size == 1 && methods[0] == :all
-      end
-  
-      def sections_is_an_array?
-        @sections.is_a?(Array)
-      end
-  
-      def one_to_one_match_for_sections_and_subtitles?
-        @sections.size % 2 == 0
-      end
-
-      def valid_types?
-        !(@sections.first.is_a?(String) || @sections.all? { |section| section.is_a?(String) })
       end
     end
   end
